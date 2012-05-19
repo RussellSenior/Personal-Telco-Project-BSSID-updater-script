d093db4686eb83db110e72dfd8e3cf22ad269c9b
Russell Senior <russell@personaltelco.net>
2012-05-19 16:40:03 -0700

    initial code dump of russell's hacky script and associated files
    Possibly derived in part from a script from jason@personaltelco.net

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ Personal-Telco-Project-BSSID-updater-script
 ===========================================
 
 A perl script that automates the scraping of PTP related network data from wigle.net
+
+The script is currently broken, in that it fails to follow up query results that return
+only the first 100 responses.  One of the queries currently returns about 800.  The script 
+needs to grab the "next" link until there are no more results.
+
+To function, the user must provide their own auth cookie string and sid code near the top 
+of the script.  These can be found by registering a wigle account and then sniffing a 
+browser sesssion.  It may be necessary or helpful to turn off gzip compression in your 
+browser prior to doing so, unless wireshark has become more clever than when I did it last.
+
+The ptp-bssid-with-notes file is a tab-delimited version the current state of our node data
+suitable for integration with an existing wiki page:
+
+  https://personaltelco.net/wiki/PersonalTelcoBssids
+
+Yes, this is incredibly hacky.  However, it was effective.
---
+++ ptp-bssid-update.pl
@@ -0,0 +1,148 @@
+#!/usr/bin/perl
+
+#use strict;
+use Socket;
+
+my ($cookie,$query,@date,$datestr,$port,$iaddr,$paddr,$proto,$line);
+
+# To use this script, discover your own auth cookie and sid and include customized expressions as commented out below:
+#$cookie='phpbb2mysql_data=a%3A2%3A%7Bs%3A11%3A%22autologinid%22%3Bs%3A0%3A%22%22%3Bs%3A6%3A%22userid%22%3Bi%3A-1%3B%7D; auth=$YOURUSERNAME%3A786538655%3A1187426809%3AIAe%2FsXTUPOUb0C7sEpZYHw';
+#phpbb2mysql_sid=$32characterHexString
+
+$query = "POST /gps/gps/main/confirmquery/ HTTP/1.1
+Host: wigle.net
+Accept: text/plain
+Keep-Alive: 300
+Connection: keep-alive
+Cookie: $cookie
+Content-Type: application/x-www-form-urlencoded";
+
+@date = localtime;
+
+$datestr = sprintf("%04d%02d%02d-%02d%02d%02d",
+                   $date[5]+1900,$date[4],$date[3],$date[2],$date[1],$date[0]);
+
+open(CACHE,">/tmp/ptp-bssid-update-html-$datestr") or die "open: $!";
+open(RESULT,">ptp-bssid-update-download-$datestr") or die "open: $!";
+
+$proto = getprotobyname('tcp');
+$port = getservbyname('http', 'tcp');
+$iaddr = inet_aton("wigle.net");
+
+$paddr = sockaddr_in($port,$iaddr);
+
+# query www.personaltelco
+
+print "debug: query www.personaltelco\n";
+
+socket(SOCK,PF_INET,SOCK_STREAM,$proto) or die "socket: $!";
+connect(SOCK,$paddr) or die "connect: $!";
+select(SOCK); $| = 1; select(STDOUT); # turns on autoflush for SOCK
+print SOCK <<EOF;
+$query
+Content-Length: 34
+
+ssid=www.personaltelco&Query=Query
+EOF
+
+shutdown(SOCK, 1);
+while (<SOCK>) {
+    print CACHE $_;
+    if (/^<td>/) {
+        s|</td><td>|\t|g;
+        s|<td>||;
+        s|</td></tr>||;
+        s|&nbsp;||g;
+        print RESULT $_;
+        chomp;
+        ($netid,$network) = split /\t/, $_, 2;
+        $netid =~ tr/a-z/A-Z/;
+        $data{$netid} = $network;
+    }
+}
+close(SOCK);
+
+
+print "debug: query personaltelco\n";
+
+# query personaltelco
+socket(SOCK,PF_INET,SOCK_STREAM,$proto) or die "socket: $!";
+connect(SOCK,$paddr) or die "connect: $!";
+select(SOCK); $| = 1; select(STDOUT); # turns on autoflush for SOCK
+print SOCK <<EOF;
+$query
+Content-Length: 30
+
+ssid=personaltelco&Query=Query
+EOF
+
+shutdown(SOCK, 1);
+while (<SOCK>) {
+    print CACHE $_;
+    if (/^<td>/) {
+        s|</td><td>|\t|g;
+        s|<td>||;
+        s|</td></tr>||;
+        s|&nbsp;||g;
+        print RESULT $_;
+        chomp;
+        ($netid,$network) = split /\t/, $_, 2;
+        $netid =~ tr/a-z/A-Z/;
+        $data{$netid} = $network;
+    }
+}
+close(SOCK);
+
+open(OLD,"ptp-bssid-with-notes") or die "open: $!";
+
+while(<OLD>) {
+    chomp;
+    ($bssid,@old) = split /\t/;
+    $bssid =~ tr/a-z/A-Z/;
+    $note{$bssid} = $old[$#old];
+    if (! defined($data{$bssid})) {
+        # if no new data yet, query by bssid
+
+        print "debug: querying $bssid with $note{$bssid}";
+
+        socket(SOCK,PF_INET,SOCK_STREAM,$proto) or die "socket: $!";
+        connect(SOCK,$paddr) or die "connect: $!";
+        select(SOCK); $| = 1; select(STDOUT); # turns on autoflush for SOCK
+        print SOCK <<EOF;
+$query
+Content-Length: 35
+
+netid=$bssid&Query=Query
+EOF
+
+        shutdown(SOCK, 1);
+        while (<SOCK>) {
+            print CACHE $_;
+            if (/^<td>/) {
+                s|</td><td>|\t|g;
+                s|<td>||;
+                s|</td></tr>||;
+                s|&nbsp;||g;
+                print RESULT $_;
+                chomp;
+                ($netid,$network) = split /\t/, $_, 2;
+                $netid =~ tr/a-z/A-Z/;
+                $data{$netid} = $network;
+            }
+        }
+        close(SOCK);
+    }
+}
+
+foreach $key (keys %data) { $merged{$key}++; }
+
+foreach $key (keys %note) { $merged{$key}++; }
+
+foreach $key (sort keys %merged) {
+    print join "\t", $key, $data{$key}, $note{$key};
+    print "\n";
+}
+
+close(CACHE);
+
+exit;
